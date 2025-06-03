eda347d0c16c4b8846b0c094b8595e9f1e6af545
Douglas Domotor <dmtr82do@gmail.com>
2025-06-03 19:15:43 -0300

    Adicionando função idade

---
+++ tests/test_idade.py
@@ -0,0 +1,8 @@
+from src.idade import data
+
+print("Testando 01/06/2000, a idade sera 25", data("01/06/2000") == 25)
+print("Testando 01/06/2000, a idade sera 24", data("01/06/2000") == 24)
+print("Testando 10/06/2000, a idade sera 24", data("10/06/2000") == 24)
+print("Testando 10/06/2000, a idade sera 25", data("10/06/2000") == 25)
+
+
