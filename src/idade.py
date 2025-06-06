6814061c2cb87f78a2376b838d1846616f561810
Douglas Domotor <dmtr82do@gmail.com>
2025-06-06 11:55:57 -0300

    Editando variavel data_hoje

--- src/idade.py
+++ src/idade.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, date
 
 def data(data_nascimento): 
     nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y").date()
-    data_hoje =  datetime.today().date() 
+    data_hoje =  date.today() 
     idade = data_hoje.year - nascimento.year
     if (data_hoje.month, data_hoje.day) < (nascimento.month, nascimento.day):
         idade -= 1
