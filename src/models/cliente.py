2b0949c667e49c9a0063ce16c972eac9d2ec7e89
Douglas Domotor <dmtr82do@gmail.com>
2025-07-11 16:26:00 -0300

    Implementando ordens de compra e venda

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -1,10 +1,12 @@
 from utils.calculadora_idade import CalculadoraIdade
+from models.acao import Acao
 
 class Cliente:
     def __init__(self, nome, data_nascimento):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.acoes = []
+        self.ordens = []
         
     def fale_seu_nome(self):
         return f"Meu nome é {self.nome}"
@@ -20,3 +22,31 @@ class Cliente:
         for acao in self.acoes:
             saldo += acao.saldo()
         return saldo
+    
+    def executar_ordem(self, ordem):
+        if ordem.tipo == 'compra':
+            for acao in self.acoes:
+                 if acao.codigo == ordem.codigo:
+                    acao.quantidade += ordem.quantidade
+                    break
+            else:
+                nova_acao = Acao(ordem.codigo, ordem.codigo, " ", ordem.valor, ordem.quantidade)
+                self.acoes.append(nova_acao)
+
+        elif ordem.tipo == "venda":
+            for acao in self.acoes:
+                if acao.codigo == ordem.codigo:
+                    if acao.quantidade >= ordem.quantidade:
+                        acao.quantidade -= ordem.quantidade
+                        if acao.quantidade == 0:
+                            self.acoes.remove(acao)
+                    else:
+                        print(f"Erro: Você só possui {acao.quantidade} ações.")
+                    break
+            else:
+                print("Erro: Ação não encontrada na carteira para venda.")
+
+        else:
+            print("Tipo de ordem inválido. Use 'compra' ou 'venda'.")
+
+        self.ordens.append(ordem)                        
--- tests/models/test_cliente.py
+++ tests/models/test_cliente.py
@@ -1,5 +1,6 @@
 from models.cliente import Cliente
 from models.acao import Acao
+from models.ordem import Ordem
 from freezegun import freeze_time
 import pytest
 
@@ -28,3 +29,33 @@ def test_saldo():
     petr = Acao("PETR", "PETR4", "Petróleo", 30.00, 200)
     cliente.adicionar_acao(petr)
     assert cliente.saldo() == 11000
+
+def test_executar_ordem():
+    cliente = Cliente("Douglas", "05/11/1982")
+
+    ordem_compra1 = Ordem('compra', 'VALE3', 100, 45.00)
+    cliente.executar_ordem(ordem_compra1)
+
+    acao = cliente.acoes[0]
+
+    assert acao.codigo == "VALE3"
+    assert acao.quantidade == 100
+    assert acao.saldo() == 4500
+
+    ordem_compra2 = Ordem('compra', 'VALE3', 10, 45.00)
+    cliente.executar_ordem(ordem_compra2)
+
+    assert acao.quantidade == 110
+    assert acao.saldo() == 4950
+
+    ordem_venda1 = Ordem('venda', 'VALE3', 10, 50.00)
+    cliente.executar_ordem(ordem_venda1)
+
+    assert acao.quantidade == 100
+    assert acao.saldo() == 4500
+
+    ordem_venda_inexistente = Ordem('venda', 'PETR3', 100, 30.00)
+    cliente.executar_ordem(ordem_venda_inexistente)
+
+    codigos = [a.codigo for a in cliente.acoes]
+    assert 'PETR3' not in codigos   
